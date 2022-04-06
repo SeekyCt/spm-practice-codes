75735638058a1167a94e22e4a8ad57c536ccf1f8
Seeky <58006653+SeekyCt@users.noreply.github.com>
2022-04-06 15:20:48 +0100

    Expand ipc.h

--- include/wii/ipc.h
+++ include/wii/ipc.h
@@ -4,10 +4,17 @@
 
 namespace wii::IPC {
 
+struct Ioctlv
+{
+    void * data;
+    u32 len;
+};
+
 extern "C" {
 
 s32 IOS_Open(const char * path, s32 mode);
-s32 IOS_Close(int fd);
+s32 IOS_Close(s32 fd);
+s32 IOS_Ioctlv(s32 fd, s32 command, s32 inCount, s32 outCount, Ioctlv * vecs);
 
 }
 
