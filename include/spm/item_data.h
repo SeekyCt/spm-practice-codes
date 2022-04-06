94e369155b64f6b9c9a74331e13564d67e61fb62
Seeky <58006653+SeekyCt@users.noreply.github.com>
2022-04-06 15:19:31 +0100

    Expand item_data.h

--- include/spm/item_data.h
+++ include/spm/item_data.h
@@ -17,12 +17,23 @@ struct ItemData
 {
     const char * itemName;
     s16 iconId;
-    u8 unknown_0x6[0x8 - 0x6];
+    u16 pad_0x6;
     const char * animPoseName;
-    u8 unknown_0xc[0x10 - 0xc];
+    const char * animName;
     const char * nameMsg;
     const char * descMsg;
-    u8 unknown_0x18[0x2c - 0x18];
+    u16 buyPrice;
+    u16 sellPrice;
+    s16 hpGain;
+    s16 xpGain;
+    u8 cardBagChance;
+    u8 cardShopChance;
+    u16 cardShopBonusSeq;
+    u16 cardShopMinGSW0;
+    bool cardShopBlockDuplicate;
+    u8 dispFlags;
+    s16 tribe;
+    s16 sortValue;
 };
 static_assert(sizeof(ItemData) == 0x2c);
 
