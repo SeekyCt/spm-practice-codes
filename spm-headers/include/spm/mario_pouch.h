7128aa490cce0b14fbf30c1ce78cb12a62808169
Seeky <58006653+SeekyCt@users.noreply.github.com>
2025-01-02 00:27:56 +0000

    git subrepo pull --force spm-headers
    subrepo:
      subdir:   "spm-headers"
      merged:   "a8b9998"
    upstream:
      origin:   "https://github.com/SeekyCt/spm-headers.git"
      branch:   "master"
      commit:   "a8b9998"
    git-subrepo:
      version:  "0.4.9"
      origin:   "???"
      commit:   "???"

--- spm-headers/include/spm/mario_pouch.h
+++ spm-headers/include/spm/mario_pouch.h
@@ -10,13 +10,20 @@
 
 CPP_WRAPPER(spm::mario_pouch)
 
+#define POUCH_KEY_ITEM_MAX 32
+#define POUCH_USE_ITEM_MAX 10
+#define POUCH_SHOP_ITEM_MAX 32
+#define POUCH_CHAR_ITEM_MAX 4
+#define POUCH_FAIRY_ITEM_MAX 16
+
 USING(wii::os::OSTime)
+USING(wii::mtx::Vec3)
 
 typedef struct
 {
 /* 0x0 */ bool selectable;
 /* 0x1 */ bool selected;
-/* 0x2 */ s16 itemType;
+/* 0x2 */ u16 itemType;
 } PouchCharOrPixlInfo;
 SIZE_ASSERT(PouchCharOrPixlInfo, 0x4)
 
@@ -24,9 +31,24 @@ typedef struct
 {
 /* 0x00 */ s32 characterId;
 /* 0x04 */ s32 score;
-/* 0x08 */ u8 unknown_0x8[0x18 - 0x8];
+/* 0x08 */ Unk32 unknown_0x8;
+/* 0x0C */ Unk32 unknown_0xc;
+/* 0x10 */ u8 unknown_0x10;
+/* 0x11 */ u8 unknown_0x11[0x18 - 0x11];
 } PouchMinigameScore;
 SIZE_ASSERT(PouchMinigameScore, 0x18)
+
+typedef struct
+{
+/* 0x00 */ Unk32 unknown_0x0;
+/* 0x04 */ s32 unknown_0x4;
+/* 0x08 */ OSTime unknown_0x8;
+/* 0x10 */ Unk32 unknown_0x10;
+/* 0x14 */ Unk32 unknown_0x14;
+/* 0x18 */ u8 unknown_0x18;
+/* 0x19 */ u8 unknown_0x19[0x20 - 0x19];
+} PouchUnk;
+SIZE_ASSERT(PouchUnk, 0x20)
 
 /*
     This struct is copied in full to & from the save file
@@ -41,14 +63,14 @@ typedef struct
 /* 0x014 */ s32 flipTimer;
 /* 0x018 */ s32 xp;
 /* 0x01C */ s32 coins;
-/* 0x020 */ s16 keyItem[32]; // key items
-/* 0x060 */ s16 useItem[10]; // usable items
-/* 0x074 */ s16 shopItem[32]; // items stored by the player at a shop
-/* 0x0B4 */ PouchCharOrPixlInfo characters[4];
-/* 0x0C4 */ PouchCharOrPixlInfo pixls[16];
+/* 0x020 */ u16 keyItem[POUCH_KEY_ITEM_MAX]; // key items
+/* 0x060 */ u16 useItem[POUCH_USE_ITEM_MAX]; // usable items
+/* 0x074 */ u16 shopItem[POUCH_SHOP_ITEM_MAX]; // items stored by the player at a shop
+/* 0x0B4 */ PouchCharOrPixlInfo characters[POUCH_CHAR_ITEM_MAX];
+/* 0x0C4 */ PouchCharOrPixlInfo pixls[POUCH_FAIRY_ITEM_MAX];
 /* 0x104 */ s32 shopPoints;
 /* 0x108 */ u32 shopPointRewardsCollected; // bit mask, index 1 << i
-/* 0x10C */ s8 catchCards[256]; // value is the amount currently owned, index is item id
+/* 0x10C */ u8 catchCards[256]; // value is the amount currently owned, index is item id
                                 // minus the first card item id (282)
 /* 0x20C */ u8 unknown_0x20c[0x30c - 0x20c];
 
@@ -66,9 +88,10 @@ typedef struct
 
 /* 0x348 */ s32 charmsRemaining; // Merlee charms remaining from current purchase
 /* 0x34C */ s32 killsBeforeNextCharm; // kills before the next Merlee charm triggers
-/* 0x350 */ u8 unknown_0x350[0x368 - 0x350];
+/* 0x350 */ Vec3 unknown_0x350;
+/* 0x35C */ Vec3 unknown_0x35c;
 /* 0x368 */ PouchMinigameScore minigameScores[4][5];
-/* 0x548 */ u8 unknown_0x548[0x688 - 0x548];
+/* 0x548 */ PouchUnk unknown_0x548[2][5];
 /* 0x688 */ s32 arcadeTokens;
 /* 0x68C */ s32 totalCoinsCollected; // all-time coins collected
 /* 0x690 */ s32 maxJumpCombo;
@@ -80,10 +103,8 @@ SIZE_ASSERT(MarioPouchWork, 0x6a0)
 
 typedef struct
 {
-/* 0x00 */ s32 unknown_0x0;
-/* 0x04 */ s32 unknown_0x4;
-/* 0x08 */ s32 unknown_0x8;
-/* 0x0C */ s32 unknown_0xc;
+/* 0x00 */ OSTime unknown_0x0;
+/* 0x08 */ OSTime unknown_0x8;
 /* 0x10 */ s32 unknown_0x10;
 /* 0x14 */ s32 unknown_0x14;
 /* 0x18 */ OSTime unknown_0x18;
@@ -104,6 +125,7 @@ MarioPouchWork2 * pouch2GetPtr(); // inlined
     Overwritten later once a save is loaded
 */
 void pouchInit();
+void pouch2Init(); // inlined
 void pouchReInit();
 
 /*
@@ -147,6 +169,7 @@ s32 pouchGetMaxHp();
 /*
     Get/add to the player's xp
 */
+void pouchSetXp(s32 xp); // inlined
 s32 pouchGetXp();
 void pouchAddXp(s32 increase);
 
@@ -162,7 +185,7 @@ void pouchAddCoin(s32 increase); // increases totalCoinsCollected
     Adds the specified item to its corresponding inventory (can't be used for shop items)
     Returns whether it was successfully added
 */
-bool pouchAddItem(s16 itemId);
+bool pouchAddItem(s32 itemId);
 
 /*
     Checks whether the specified item id is in its corresponding inventory
@@ -230,7 +253,7 @@ bool pouchCheckPixlSelected(s16 itemId);
 /*
     Returns the id of the currently selected pixl
 */
-s16 pouchGetCurPixl();
+u16 pouchGetCurPixl();
 
 /*
     Makes a pixl selected by id, if it exists in a slot
