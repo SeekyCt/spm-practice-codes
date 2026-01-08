ea36736e48ee1efef8ee2cfe9d966dd5c036eab6
SeekyCt <58006653+SeekyCt@users.noreply.github.com>
2026-01-08 19:23:43 +0000

    Cleanup seq_load.h

--- spm-headers/include/spm/seq_load.h
+++ spm-headers/include/spm/seq_load.h
@@ -2,15 +2,10 @@
 
 #include <common.h>
 #include <spm/seqdrv.h>
-#include <spm/evtmgr.h>
-#include <spm/filemgr.h>
-
-#include <wii/gx.h>
 
 CPP_WRAPPER(spm::seq_load)
 
 USING(spm::seqdrv::SeqWork)
-USING(spm::evtmgr::EvtEntry)
 
 UNKNOWN_FUNCTION(initNewSaveFile)
 void seq_loadInit(SeqWork * work);
